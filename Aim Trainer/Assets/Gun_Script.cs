ca0c94ef68b7ce7a406d58001578d6f588b97dc6
JarredNaicker <wts9778@autuni.ac.nz>
2020-09-11 23:00:49 +1200

    Gun_Shot
    Setting up the gun to object targetting.

---
+++ Aim Trainer/Assets/Gun_Script.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class Gun_Script : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if( Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            //output object shot
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
