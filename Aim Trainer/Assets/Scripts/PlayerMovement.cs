51f8e7cf90cafcfab2fb53f5b2fe504c60822954
JaspherVillanueva <69949305+JaspherVillanueva@users.noreply.github.com>
2020-09-19 01:53:41 +1200

    Add jump functionality

--- Aim Trainer/Assets/Scripts/PlayerMovement.cs
+++ Aim Trainer/Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-
     public float speed = 12f;
+    public float jumpHeight = 3f;
 
     public float gravity = -9.81f;
-
     public Transform groundCheck;
-
     public float groundDistance = 0.4f;
-
     public LayerMask groundMask;
 
     Vector3 velocity;
@@ -41,6 +38,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
