0a0c333b00f45180e6eb681fddc3231d123ee7e7
JarredNaicker <wts9778@autuni.ac.nz>
2020-09-23 23:28:54 +1200

    ScoreScript, EnemyGeneration, Crosshair
    added 2 scripts to generate enemy and count/ output the score

---
+++ Aim Trainer/Assets/Scripts/GenerateEnemies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    public int maxEnemies = 5;
+    public int xPos;
+    public int zPos;
+    public int enemyCount = 0;
+    public GameObject theTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    // Update is called once per frame
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < maxEnemies)
+        {
+            xPos = Random.Range(-40, 80);
+            zPos = Random.Range(-10, 40);
+            Instantiate(theTarget, new Vector3(xPos, 2, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+}
--- Aim Trainer/Assets/Scripts/Target.cs
+++ Aim Trainer/Assets/Scripts/Target.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class Target : MonoBehaviour
 {
     public float health = 50f;
+    private int TargetScore = 10;
 
     public void TakeDamage (float amount)
     {
@@ -15,6 +18,8 @@ public class Target : MonoBehaviour
 
     void Die()
     {
+        ScoreScript.scoreValue += TargetScore;
         Destroy(gameObject);
+        Debug.Log("Target Destroyed");
     }
 }
