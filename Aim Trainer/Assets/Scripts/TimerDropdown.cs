6da1097ae506db8de6c7e0b58378b65841fd85af
JaspherVillanueva <69949305+JaspherVillanueva@users.noreply.github.com>
2020-10-23 20:43:07 +1300

    Game Over Screen

---
+++ Aim Trainer/Assets/Scripts/TimerDropdown.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerDropdown : MonoBehaviour
+{
+    public int time;
+
+    public void HandleInputData(int val)
+    {
+        if (val == 0)
+        {
+            time = 60;
+            Debug.Log(time);
+        }
+        if (val == 1)
+        {
+            time = 600;
+            Debug.Log(time);
+        }
+        if (val == 2)
+        {
+            time = 1200;
+            Debug.Log(time);
+        }
+    }
+
+
+}
