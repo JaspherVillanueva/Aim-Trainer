e41f15a0476b75674c89a33494f87b03b269a87a
thanh13574 <69949702+thanh13574@users.noreply.github.com>
2020-10-29 17:23:41 +1300

    made new script
    new script for moving target/target generation

---
+++ Aim Trainer/Assets/Scripts/MovingTargetGenerate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+using Debug = UnityEngine.Debug;
+
+public class MovingTargetGenerate : MonoBehaviour
+{
+    public int CloseEnemies = 1;
+
+    public GameObject closeTarget_Obj;
+
+    public static float closeTarget_radius = 20.0f;
+
+    private int xPos;
+    private int zPos;
+    private int yPos = 1;
+    private Vector3 Center = new Vector3(50, 5, -50);
+    private int enemyCount;
+    private String sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            StartCoroutine(SpawnCircleOfEnemies(CloseEnemies, Center, closeTarget_Obj, closeTarget_radius));
+    }
+
+
+    IEnumerator SpawnCircleOfEnemies(int maxEnemies, Vector3 center, GameObject Target, float radius)
+    {
+        //while enemy count <= maxEnemy
+        for (int counter = 1; counter <= maxEnemies; counter++)
+        {
+            Vector3 pos = RandomCircle(center, radius);
+            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center - pos);
+            //spawn the object
+            Instantiate(Target, pos, rot);
+            enemyCount++;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    //used to get random vector from a circle
+    Vector3 RandomCircle(Vector3 Center, float radius)
+    {
+        float angle = Random.value * 360;
+        Vector3 pos;
+        pos.x = Center.x + radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+        pos.z = Center.z + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        pos.y = Center.y;
+        return pos;
+    }
+}
---
+++ Aim Trainer/Assets/Scripts/TargetMoving.cs
@@ -0,0 +1,74 @@
+﻿using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = UnityEngine.Debug;
+using Random = UnityEngine.Random;
+using System;
+/*
+ * This class is used to instatiate 
+ * Targets for the player to shoot
+ * Targets either take damage,
+ * or they die due to no more health
+ */
+public class TargetMoving : MonoBehaviour
+{
+    //instantiating variables
+    public float health = 50f;
+    public int TargetScore = 10;
+    public GameObject target;
+    public int EnemyDistance;
+
+    private MovingTargetGenerate Generator;
+    private String sceneName;
+
+    private Vector3 Center = new Vector3(50, 5, -50);
+
+    private void Start()
+    {
+        //get the active scene
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+        Generator = GameObject.FindWithTag("EnemyGenerate").GetComponent<MovingTargetGenerate>();
+    }
+
+    //when target is shot by player
+    //target will take damage until it reaches 0
+    public void TakeDamage(float damage)
+    {
+        GameManager.TargetsHit++;
+        //decrease health
+        health -= damage;
+        //if health below 0
+        if (health <= 0f)
+        {
+            ScoreScript.scoreValue += TargetScore;
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 pos = RandomCircle(Center, 20f);
+        
+        target.transform.position += pos;
+
+        Wait(1);
+    }
+
+    Vector3 RandomCircle(Vector3 Center, float radius)
+    {
+        float angle = Random.value * 360;
+        Vector3 pos;
+        pos.x = Center.x + radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+        pos.z = Center.z + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        pos.y = Center.y;
+        return pos;
+    }
+
+    IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+
+}
